2d8f73a3d7cf746e8362b0cdfd15fd04f5e5f042
21oglethorpe <jc2376@scarletmail.rutgers.edu>
2021-04-08 05:43:53 +0000

    Update pixel_pusher.vhd

--- pixel_pusher.vhd
+++ pixel_pusher.vhd
@@ -17,22 +17,26 @@ end pixel_pusher;
 architecture Behavioral of pixel_pusher is
 signal addrr : std_logic_vector(17 downto 0):= (others => '0');
 begin
-
 addr <= addrr;
 process(clk)
 begin
+
     if rising_edge(clk) then
-    if VS = '0' then
-            addrr <= (others => '0');
+    
+    
+        if VS = '0' then
+                addrr <= (others => '0');
             
-    elsif(en = '1' AND vid = '1' AND unsigned(hcount) < 480) then
-            addrr <= std_logic_vector(unsigned(addrr)+1);
-            R <= pixel(7 downto 5) & "00";
-            G <= pixel(4 downto 2) & "000";
-            B <= pixel (1 downto 0) & "000";
-    else
-        R <= (others => '0');         B <= (others => '0');        G <= (others => '0');
+        elsif(en = '1' AND vid = '1' AND unsigned(hcount) < 480) then
+                addrr <= std_logic_vector(unsigned(addrr)+1);
         end if;
+        if(en = '1' AND vid = '1' AND unsigned(hcount) < 480) then
+                R <= pixel(7 downto 5) & "00";
+                G <= pixel(4 downto 2) & "000";
+                B <= pixel (1 downto 0) & "000";
+        else
+            R <= (others => '0');         B <= (others => '0');        G <= (others => '0');
+    end if;
     end if;
     
 end process;
